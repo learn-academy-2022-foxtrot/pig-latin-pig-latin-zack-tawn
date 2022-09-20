2dd9d958e937c82a32d7cfaa12c1e2f60401e078
tawnchan <tawnchan@gmail.com>
2022-09-20 11:14:44 -0700

    Vowel Functionality Complete

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import butcherPigImage from './assets/butcherPig.jpeg'
 const App = () => {
 
   // ACTION ITEM: to make the development process easier there are some preassigned words in the input field, when you are ready for your full user experience delete the test words passed to useState and pass an empty string
-  const [userInput, setUserInput] = useState("apple through queen squeal fry fluent")
+  const [userInput, setUserInput] = useState("")
   const [inputTranslated, setInputTranslated] = useState("")
 
   // ACTION ITEM: the "myPigLatinCodeHere" function is where you will put your logic to translate the sentence entered by the user into Pig Latin
@@ -32,7 +32,10 @@ const App = () => {
       console.log("vowelsArray:", vowelsArray)
 
       // ACTION ITEM: your Pig Latin logic goes here!
-
+      // want vowelsArray.includes()
+      if (vowelsArray.includes(eachWord.charAt(0)))  {
+          eachWord = eachWord.concat("way")
+      }
     
 
       // ACTION ITEM: this return will be the output of your Pig Latin'd code
